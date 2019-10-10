5272486e2c0b9ab268222f9837a55f84a5cbe17d
TimUK <timj178@gmail.com>
2019-10-10 20:40:17 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+﻿import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Blank from "./components/Blank";
+//TODO Web Template Studio: Add routes for your new pages here.
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          
+          <Route path = "/Blank" component = { Blank } />
+		  <Route exact path="/" component={Home}/>
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,43 @@
+﻿import React from "react";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+	paper: {
+		marginTop:"3vh",
+	  padding: theme.spacing(3, 2),
+	  width:"50vw",
+	  borderColor:"#212529",
+	  //color:"#fff",
+	  marginLeft:"auto",
+	  marginRight:"auto"
+	},
+	root:{
+		backgroundColor:"#999999",
+		padding:"1px",
+		maxHeight:"71vh",
+		overflowY:"scroll"
+	}
+  }));
+
+export default function Blank() {
+	var posts = [
+		{title:"hello",author:"Tim",body:"this is my first post"},
+		{title:"Disney Frozen",author:"Anna",body:"Do you wanna build a snowman?"},
+		{title:"More post?",author:"Postman Pat",body:"I can't deliver all these posts!"},
+		{title:"Are you one of the Winkelvoss twins??",author:"Mark Zuckerberg",body:"You have stolen Facebook! You didn't create Facebook!"}
+	];
+	const classes = useStyles();
+  return (
+  	<div id="mainContent" className={classes.root}>
+		{posts.map(post=>
+			<Paper className={classes.paper}>
+				<h1>{post.title}</h1>
+				{post.body}<br/>
+				<sub>- {post.author}</sub>
+			</Paper>
+		)}  
+		  
+	</div>
+  );
+}
